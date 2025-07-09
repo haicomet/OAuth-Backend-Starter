8191c0ebe2c19d6f4dc3b5ee952ac25cf7fa69ec
Joseph Collado <sourpina@gmail.com>
2025-07-09 13:40:27 -0400

    Mounted users router to /users

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const ducksRouter = require("./ducks");
+const usersRouter = require("./users");
 
 router.use("/ducks", ducksRouter);
+router.use("/users", usersRouter);
 
 module.exports = router;
